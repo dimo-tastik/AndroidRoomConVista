9124b72734f2ceeec06cd98c1cd769b6424ba78f
dIMITRI <dtomassob@gmail.com>
2022-11-13 23:06:20 +0100

    creado el repositorio

---
+++ app/src/main/java/com/example/contactosapp/repositorios/RepositorioContactosTelefonoDB.java
@@ -0,0 +1,44 @@
+package com.example.contactosapp.repositorios;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.contactosapp.conexionBDD.BaseDatosContactos;
+import com.example.contactosapp.conexionBDD.ContactosTelefonoDAO;
+import com.example.contactosapp.conexionBDD.EntidadContacto;
+import com.example.contactosapp.conexionBDD.EntidadTelefono;
+
+import java.util.List;
+
+public class RepositorioContactosTelefonoDB {
+
+    private ContactosTelefonoDAO contactosTelefonoDAO;
+    private LiveData<List<EntidadContacto>> AllContactos;
+    private LiveData<List<EntidadTelefono>> AllTelefonos;
+
+    public RepositorioContactosTelefonoDB(Application application) {
+        BaseDatosContactos db = BaseDatosContactos.getDatabase(application);
+        contactosTelefonoDAO = db.contactoDAO();
+        AllContactos = contactosTelefonoDAO.getContactos();
+        AllTelefonos = contactosTelefonoDAO.getTelefonos();
+    }
+
+    public LiveData<List<EntidadContacto>> getAllContactos() {
+        return AllContactos;
+    }
+    public LiveData<List<EntidadTelefono>> getAllTelefonos() {
+        return AllTelefonos;
+    }
+
+    public void insertarContacto(EntidadContacto contacto) {
+        BaseDatosContactos.baseDatosEscritor.execute(() -> {
+            contactosTelefonoDAO.insertarContacto(contacto);
+        });
+    }
+    public void insertarTelefono(EntidadTelefono telefono) {
+        BaseDatosContactos.baseDatosEscritor.execute(() -> {
+            contactosTelefonoDAO.insertarTelefono(telefono);
+        });
+    }
+}
